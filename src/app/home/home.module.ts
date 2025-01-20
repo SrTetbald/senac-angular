c49e40afae84c7e264c9e367d78c2e20d84693df
SrTetbald <dev.hebert.abc@gmail.com>
2025-01-20 09:06:21 -0300

    fajlksdjf

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { HeaderModule } from '../header/header.module';
+import { FooterModule } from '../footer/footer.module';
+
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    HeaderModule,
+    FooterModule
+  ]
+})
+export class HomeModule { }
